b1fb2c3d21df1785753e21d008edb4021d1244aa
mehmetvarol <mehmt.varol@gmail.com>
2020-08-08 17:50:48 +0300

    svg dosyaları ayarlandı

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,18 @@
   <div class="home">
     <h1>Home</h1>
     <CustomText tag="p" size="small">Mehmet VAROL</CustomText>
+    <IconHome />
   </div>
 </template>
 
 <script>
 import CustomText from '@/components/CustomText'
+import IconHome from '@/assets/icons/home.svg'
 export default {
   name: 'Home',
   components: {
-    CustomText
+    CustomText,
+    IconHome
   }
 }
 </script>
