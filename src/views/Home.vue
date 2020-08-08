a9f7349bc866fc5a1b5593f63e8627bb7ff752d8
mehmetvarol <mehmt.varol@gmail.com>
2020-08-08 17:18:47 +0300

    css alt yapısı kuruldu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText tag="p" size="small">Mehmet VAROL</CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText'
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    CustomText
+  }
 }
 </script>
